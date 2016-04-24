b96e27242bd07352c2bde8bbbb61d1a69ee9e8b3
Jelte <jelte_@outlook.com>
2016-04-24 14:17:19 +0200

    a

--- src/Test.java
+++ src/Test.java
@@ -1,13 +1,10 @@
-import com.sun.deploy.config.Config;
 
-import java.io.BufferedReader;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.ConnectException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.logging.Level;
 
 @WebServlet("/test")
 public class Test extends HttpServlet {
