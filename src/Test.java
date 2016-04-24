abb01b05f0e5f44733b2cbf68966e0f3eab882d4
Jelte <jelte_@outlook.com>
2016-04-24 14:08:32 +0200

    a

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,21 @@
-/**
- * Created by Jelte on 24-4-2016.
- */
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("yaaoo!");
+import com.sun.deploy.config.Config;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ConnectException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.logging.Level;
+
+@WebServlet("/test")
+public class Test extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.getWriter().write("test");
+
     }
 }
